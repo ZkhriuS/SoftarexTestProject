9385c176012b18f09f65491206814203ea3fda69
Zakhar <seriy.zachar@yandex.by>
2023-04-29 14:33:34 +0300

    initiated project

--- Classroom/src/main/java/com/softarex/classroom/Classroom/ClassroomApplication.java
+++ Classroom/src/main/java/com/softarex/classroom/Classroom/ClassroomApplication.java
@@ -1,4 +1,4 @@
-package com.example.classroom;
+package com.softarex.classroom.Classroom;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- Classroom/src/test/java/com/softarex/classroom/Classroom/ClassroomApplicationTests.java
+++ Classroom/src/test/java/com/softarex/classroom/Classroom/ClassroomApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.classroom;
+package com.softarex.classroom.Classroom;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
